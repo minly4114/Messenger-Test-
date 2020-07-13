0c3618e9591e2456bae71dc37ad7c2989080e42d
Elizaveta <lizasemenova19980@gmail.com>
2020-07-14 00:19:58 +0300

    создание проекта, настройка авторизации

---
+++ Messenger/Messenger/Data/PostgresContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messenger.Data
+{
+    public class PostgresContext : IdentityDbContext
+    {
+        public PostgresContext(DbContextOptions<PostgresContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Authorization");
+            builder.Ignore<SelectListItem>();
+        }
+    }
+}
